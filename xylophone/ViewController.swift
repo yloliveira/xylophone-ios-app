a104476d5f2a29f651d71ccfc23b665a726c643d
Yan Oliveira <oliveira.yan02@gmail.com>
2022-08-19 13:11:22 -0300

    feat: add buttons behaviors

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer?
+    
+    @IBAction func CPressed(_ sender: UIButton) {
+        playSound(note: "C")
     }
 
+    @IBAction func DPressed(_ sender: UIButton) {
+        playSound(note: "D")
+    }
+    
+    @IBAction func EPressed(_ sender: UIButton) {
+        playSound(note: "E")
+    }
+    
+    @IBAction func FPressed(_ sender: UIButton) {
+        playSound(note: "F")
+    }
+    
+    @IBAction func GPressed(_ sender: UIButton) {
+        playSound(note: "G")
+    }
+    
+    @IBAction func APressed(_ sender: UIButton) {
+        playSound(note: "A")
+    }
+    
+    @IBAction func BPressed(_ sender: UIButton) {
+        playSound(note: "B")
+    }
+    
+    func playSound(note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
 
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
-
