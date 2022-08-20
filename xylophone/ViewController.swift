312048f6789f9c4b334cc43089d671fa8b544b33
Yan Oliveira <oliveira.yan02@gmail.com>
2022-08-20 11:06:41 -0300

    refactor: simplify the code to use only one function when buttons are pressed

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -11,32 +11,8 @@ import AVFoundation
 class ViewController: UIViewController {
     var player: AVAudioPlayer!
     
-    @IBAction func CPressed(_ sender: UIButton) {
-        playSound(note: "C")
-    }
-
-    @IBAction func DPressed(_ sender: UIButton) {
-        playSound(note: "D")
-    }
-    
-    @IBAction func EPressed(_ sender: UIButton) {
-        playSound(note: "E")
-    }
-    
-    @IBAction func FPressed(_ sender: UIButton) {
-        playSound(note: "F")
-    }
-    
-    @IBAction func GPressed(_ sender: UIButton) {
-        playSound(note: "G")
-    }
-    
-    @IBAction func APressed(_ sender: UIButton) {
-        playSound(note: "A")
-    }
-    
-    @IBAction func BPressed(_ sender: UIButton) {
-        playSound(note: "B")
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(note: (sender.titleLabel?.text)!)
     }
     
     func playSound(note: String) {
